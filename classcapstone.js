232147425872b04f8f46365ca80b26640babf871
Ahmad2025755 <ahmad.alain2022@gmail.com>
2025-08-13 15:14:47 +0300

    Add files via upload

--- classcapstone.js
+++ classcapstone.js
@@ -3,16 +3,16 @@ function carousel() {
   var i;
   var x = document.getElementsByClassName("myslides");
 
-  for (i = 0; i < 3; i++) {  
+  for (i = 0; i < x.length; i++) {``
     x[i].style.display = "none";
   }
 
-  index++; 
+  index++;
   if (index > x.length) {
     index = 1;
   }
 
-  x[i].style.display = "block";
+  x[index - 1].style.display = "block";
 
   setTimeout(carousel, 2000);
 }
