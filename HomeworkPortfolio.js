6cf1859f341185413743e1f9f66a1566c222fe8c
Ahmad2025755 <ahmad.alain2022@gmail.com>
2025-08-13 22:33:42 +0300

    Add files via upload

---
+++ HomeworkPortfolio.js
@@ -0,0 +1,21 @@
+function About() {
+
+document.getElementById("Aboutme-info").innerHTML =
+"Hello! My name is Ahmad Abdullah and I study in grade 5. This year, I will be transfering to grade 6. I live in Pakistan. I am 11 years old, and my best friends names are Sabaoon, Abdullah, and Ibaad!"
+
+}
+
+function Friends() {
+
+document.getElementById("AboutFriends-info").innerHTML =
+"I have 3 different friends, and their name is Ibaad, Sabaoon, and Abdullah. They all are around 10-11 years old. They all study with me at school, and we play Football together. We are eachothers best friends, and are the best Squad in the whole class."
+
+}
+
+function Family() {
+
+document.getElementById("AboutFamily-info").innerHTML =
+
+"I have 1 brother, and a mom and a dad. My dads name is Shahid and is 42 years old. My moms name is Faeza and is 35 years old, and my brothers name is Umar and is 10 years old. My brother was born on 2015, and we go to the same school. My dad lives in qatar, and we live in Pakistan, but we often come to Qatar for summer and winter vacations."
+
+}
